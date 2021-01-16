0748801398092f03e0c96452af27011bde8219dc
tommR1 <76653019+tommR1@users.noreply.github.com>
2021-01-16 02:09:31 +0100

    Update CryptoNoteConfig.h

--- lib/Global/CryptoNoteConfig.h
+++ lib/Global/CryptoNoteConfig.h
@@ -83,7 +83,7 @@ const size_t   DIFFICULTY_WINDOW                              = EXPECTED_NUMBER_
 const size_t   DIFFICULTY_CUT                                 = 60;  // timestamps to cut after sorting
 const size_t   DIFFICULTY_LAG                                 = 15;  // !!!
 static_assert(2 * DIFFICULTY_CUT <= DIFFICULTY_WINDOW - 2, "Bad DIFFICULTY_WINDOW or DIFFICULTY_CUT");
-const size_t DEFAULT_DIFFICULTY                               = 10000;
+const size_t DEFAULT_DIFFICULTY                               = 100;
 const size_t FIXED_DIFFICULTY                                 = 0;
 
 static constexpr uint64_t POISSON_CHECK_TRIGGER               = 10;   // Reorg size that triggers poisson timestamp check
